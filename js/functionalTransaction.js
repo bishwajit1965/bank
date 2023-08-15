b07448bc4d9ee4e9677619b2c7844399e1ce0988
Bishwajit Paul <paul.bishwajit09@gmail.com>
2023-08-15 17:36:43 +0600

    committed

--- js/functionalTransaction.js
+++ js/functionalTransaction.js
@@ -1,52 +1,67 @@
 /* Get the values from hard-coded text field
 ============================================= */
 function textFieldValueById(textFieldId) {
-  const textField = document.getElementById(textFieldId);
-  const textFieldString = textField.innerText;
-  const textFieldValue = parseFloat(textFieldString);
-  return textFieldValue;
+    const textField = document.getElementById(textFieldId);
+    const textFieldString = textField.innerText;
+    const textFieldValue = parseFloat(textFieldString);
+    return textFieldValue;
 }
 
 /* Get the values from input field
-=================================== */
+================================================= */
 function inputFieldValuesById(inputId) {
-  const inputField = document.getElementById(inputId);
-  const stringInputField = inputField.value;
-  const inputFieldValue = parseFloat(stringInputField);
-  inputField.value = "";
-  return inputFieldValue;
+    const inputField = document.getElementById(inputId);
+    const stringInputField = inputField.value;
+    if (stringInputField == "") {
+        alert("Empty deposit input field !!!");
+    } else {
+        const inputFieldValue = parseFloat(stringInputField);
+        inputField.value = "";
+        return inputFieldValue;
+    }
 }
 
 /* Set the text element value
-================================== */
+================================================= */
 function updateTextElementValueById(elementId, newValue) {
-  const textElement = document.getElementById(elementId);
-  textElement.innerText = newValue;
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = parseFloat(newValue);
 }
 
 /* For deposit calculation
 ========================== */
-document.getElementById("btn-deposit").addEventListener("click", function () {
-  const previousDeposits = textFieldValueById("deposit");
-  const presentDepositAmount = inputFieldValuesById("deposit-money");
-  const presentDeposit = previousDeposits + presentDepositAmount;
-  updateTextElementValueById("deposit", presentDeposit);
-
-  const previousBalance = textFieldValueById("initial-balance");
-  const presentBalance = previousBalance + presentDepositAmount;
-  updateTextElementValueById("initial-balance", presentBalance);
+document.getElementById("btn-deposit").addEventListener("click", function() {
+    const previousDeposits = textFieldValueById("deposit");
+    const presentDepositAmount = inputFieldValuesById("deposit-money");
+    const presentDeposit = previousDeposits + presentDepositAmount;
+    updateTextElementValueById("deposit", presentDeposit.toFixed(2));
+    const previousBalance = textFieldValueById("initial-balance");
+    const presentBalance = previousBalance + presentDepositAmount;
+    updateTextElementValueById("initial-balance", presentBalance.toFixed(2));
 });
 
 /* For withdraw calculation
 =========================== */
-document.getElementById("btn-withdraw").addEventListener("click", function () {
-  const presentWithdrawAmount = inputFieldValuesById("withdrawn-money");
-  const previousBalance = textFieldValueById("initial-balance");
-  const previousWithdrawal = textFieldValueById("withdrawn-amount");
+document.getElementById("btn-withdraw").addEventListener("click", function() {
+    const presentWithdrawAmount = inputFieldValuesById("withdrawn-money");
+    const previousBalance = textFieldValueById("initial-balance");
+    const previousWithdrawal = textFieldValueById("withdrawn-amount");
 
-  const presentWithdrawal = previousWithdrawal + presentWithdrawAmount;
-  updateTextElementValueById("withdrawn-amount", presentWithdrawal);
-
-  const balanceAfterWithdraw = previousBalance - presentWithdrawAmount;
-  updateTextElementValueById("initial-balance", balanceAfterWithdraw);
+    if (previousBalance < presentWithdrawAmount) {
+        alert(
+            `Insufficient balance!! ${presentWithdrawAmount} is greater than balance.`
+        );
+        return;
+    } else {
+        const balanceAfterWithdraw = previousBalance - presentWithdrawAmount;
+        updateTextElementValueById(
+            "initial-balance",
+            balanceAfterWithdraw.toFixed(2)
+        );
+        const presentWithdrawal = previousWithdrawal + presentWithdrawAmount;
+        updateTextElementValueById(
+            "withdrawn-amount",
+            presentWithdrawal.toFixed(2)
+        );
+    }
 });
--- js/login.js
+++ js/login.js
@@ -2,11 +2,13 @@
 ========================== */
 document.getElementById("btn-submit").addEventListener("click", function () {
   // Step-2 Getting the input fields values email & password
+  const mailAddress = "pew@gmail.com";
+  const pass = "password";
   const emailAddress = document.getElementById("user-email");
   const email = emailAddress.value;
   const userPassword = document.getElementById("user-password");
   const password = userPassword.value;
-  if (email === "pew@gmail.com" && password === "password") {
+  if (email === mailAddress && password === pass) {
     window.location.href = "bank.html";
     alert("User is logged in!!");
   } else {
